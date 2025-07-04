9f753bd92dd9324cd855b88bdca245271e46f9cf
Erica <erica@gmail.com>
2025-07-04 20:40:44 -0300

    Deploy premium completo com Tailwind e estrutura funcional

---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,7 @@
+export { default as AdminDashboard } from './Dashboard';
+export { default as Operacoes } from './Operacoes';
+export { default as Alertas } from './Alertas';
+export { default as Financeiro } from './Financeiro';
+export { default as Usuarios } from './Usuarios';
+export { default as Afiliados } from './Afiliados';
+export { default as Parametros } from './Parametros';
---
+++ src/pages/afiliado/index.jsx
@@ -0,0 +1,4 @@
+export { default as AffiliateDashboard } from './Dashboard';
+export { default as AffiliateExtrato } from './Extrato';
+export { default as AffiliateConvite } from './Convite';
+export { default as AffiliateSaque } from './Saque';
